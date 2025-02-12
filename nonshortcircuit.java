6aabea6e441170daefc241843571f4630b2a7bf9
Athish Vasanth <120155289+athishvasanth@users.noreply.github.com>
2025-02-12 11:53:22 +0530

    Add files via upload

---
+++ nonshortcircuit.java
@@ -0,0 +1,19 @@
+public class maxofthree {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=10;
+        b=50;
+        c=100;
+        if (a>b && a>c){
+            System.out.println("a is greater");
+        } else if (b>c && b>a) {
+            System.out.println("b is greater");
+
+
+            }
+        else {
+            System.out.println("c is greater");
+
+        }
+    }
+}
