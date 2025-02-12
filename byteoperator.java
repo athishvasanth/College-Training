2e34c9592512b307b46da1c45a6d789f36f95935
Athish Vasanth <120155289+athishvasanth@users.noreply.github.com>
2025-02-12 12:01:36 +0530

    Add files via upload

---
+++ byteoperator.java
@@ -0,0 +1,23 @@
+public class datatypes {
+    public static void main(String[] args) {
+        System.out.println("operators using byte");
+        byte a,b;
+        a=10;
+        b=20;
+        byte Add= (byte )(a+b);
+        byte dif= (byte )(a-b);
+        byte mul= (byte )(a*b);
+        byte div= (byte )(a/b);
+        byte mod= (byte )(a%b);
+
+        System.out.println("Add:"+Add);
+
+
+
+        System.out.println("sub:"+dif);
+        System.out.println("mul:"+mul);
+
+        System.out.println("div:"+div);
+        System.out.println("mod:"+mod);
+    }
+}
